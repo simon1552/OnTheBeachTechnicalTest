9ed8565c26da5627bf853b6390c46110b325c63e
Simon Siu <55548012+simon1552@users.noreply.github.com>
2024-07-12 23:30:36 +0100

    Adding Holiday Search Test

---
+++ OnTheBeachTest/HolidaySearchTest/HolidaySearchTest.cs
@@ -0,0 +1,28 @@
+namespace OnTheBeachTest.HolidaySearchTest
+
+{
+    [TestFixture]
+    public class HolidaySearchTest
+    {
+        
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Search_Departing_From_MAN_Traveling_To_AGP()
+        // what I want is to test the total price of the model
+        {
+            //arrange
+            
+            
+            //act
+            
+            //assert
+        }
+        
+    
+    }
+}
+
