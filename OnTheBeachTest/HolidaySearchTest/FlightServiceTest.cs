37f092508ea8660cc7203bafcc69ac14a01f0636
Simon Siu <55548012+simon1552@users.noreply.github.com>
2024-07-19 14:38:29 +0100

    Added unit test for service

--- OnTheBeachTest/HolidaySearchTest/FlightServiceTest.cs
+++ OnTheBeachTest/HolidaySearchTest/FlightServiceTest.cs
@@ -42,6 +42,34 @@ public class FlightServiceTest
     } 
     
     [Test]
+    public void GivenFlightRepository_WhenGetFilteredFlightsWithAnyLondon_ThenReturnFilteredFlights()
+    {
+        //arrange
+        
+        var flightRepo = _autoMocker.GetMock<IFlightRepository>();
+        var flightService = _autoMocker.CreateInstance<FlightService>();
+        
+        var allFlights = _fixture.CreateMany<Flight>(10).ToList();
+        
+        var matchingFlight = _fixture.Create<Flight>();
+        matchingFlight.From = "LTN";
+        matchingFlight.To = "PMI";
+        matchingFlight.DepartureDate = "2023-06-15";
+            
+        allFlights.Add(matchingFlight);
+
+        flightRepo.Setup(f => f.GetFlights()).Returns(allFlights);
+
+        // Act
+        var flights = flightService.GetFilteredFlights("ANY London", "PMI", "2023-06-15");
+
+        // Assert
+        flights.Should().ContainSingle();
+        flights.First().Should().BeEquivalentTo(matchingFlight);
+
+    } 
+    
+    [Test]
     public void GivenFlightRepository_WhenGetFilteredFlightsWithSpecific_ThenReturnFilteredFlights()
     {
         //arrange
--- OnTheBeachTest/obj/Debug/net8.0/OnTheBeachTest.AssemblyInfo.cs
+++ OnTheBeachTest/obj/Debug/net8.0/OnTheBeachTest.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OnTheBeachTest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4847b2031141bafa16edc6df380b1150fe5269e2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+07a93047d6c9fb840b255f8d09bc8995e680a09c")]
 [assembly: System.Reflection.AssemblyProductAttribute("OnTheBeachTest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OnTheBeachTest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
